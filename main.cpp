ab684381bd9c8d1cc3876e440f8fa8385c02eb30
Nicholas Page <nicholas.page@goat.com>
2023-03-07 17:56:07 -0800

    use pointer

--- main.cpp
+++ main.cpp
@@ -431,7 +431,7 @@ int main(int argc, char* argv[]) {
             connOpts.set_password(mqtt_password);
 
             // Connect to the MQTT broker
-            mqtt_cli.connect(connOpts)->wait();
+            mqtt_cli->connect(connOpts)->wait();
             slog::info << "OK" << slog::endl;
         }
 
